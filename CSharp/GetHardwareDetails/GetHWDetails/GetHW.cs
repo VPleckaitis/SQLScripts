0035e52a635671c337781512a2a27d7509669e29
Vytautas Pleckaitis <vytautas@email.com>
2019-07-17 21:06:09 +0100

    Get HW details implementation

---
+++ CSharp/GetHardwareDetails/GetHWDetails/GetHW.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHWDetails
+{
+    internal class GetHWinfo
+    {
+        public string identifier(string wmiClass, string wmiProperty, string wmiMustBeTrue)
+        {
+            string result = "";
+            System.Management.ManagementClass mc = new System.Management.ManagementClass(wmiClass);
+            System.Management.ManagementObjectCollection moc = mc.GetInstances();
+            foreach (System.Management.ManagementObject mo in moc)
+            {
+                if (mo[wmiMustBeTrue].ToString() == "True")
+                {
+                    //Only get the first one
+                    if (result == "")
+                    {
+                        try
+                        {
+                            result = mo[wmiProperty].ToString();
+                            break;
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public string identifier(string wmiClass, string wmiProperty)
+        {
+            string result = "";
+            System.Management.ManagementClass mc = new System.Management.ManagementClass(wmiClass);
+            System.Management.ManagementObjectCollection moc = mc.GetInstances();
+            foreach (System.Management.ManagementObject mo in moc)
+            {
+                //Only get the first one
+                if (result == "")
+                {
+                    try
+                    {
+                        result = mo[wmiProperty].ToString();
+                        break;
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    public class GetDetails
+    {
+        /// <summary>
+        /// Queries WMI for list of properties, and returns result in dictionary [string,string]
+        /// </summary>
+        /// <param name="keyValuePairs">Dictionary of parameters</param>
+        /// <returns>Results in dictionary, where key = "wmiClass:wmiProperty", value = result from wmi. All exceptions will be suppressed and value set to exception message</returns>
+        public Dictionary<string, string> GetMultiDetails(Dictionary<string,string> keyValuePairs)
+        {
+            GetHWinfo HW = new GetHWinfo();
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            foreach(var keyValuePair in keyValuePairs)
+            {
+                string hwresult = "";
+                try
+                {
+                    hwresult = HW.identifier(keyValuePair.Key, keyValuePair.Value);
+                }
+                catch (Exception ex)
+                {
+                    hwresult = ex.Message;
+                }
+
+            data.Add(keyValuePair.Key+":"+ keyValuePair.Value, hwresult);
+            }
+            return data;
+        }
+        /// <summary>
+        /// Returns result for single wmi property
+        /// </summary>
+        /// <param name="wmiClass">wmi class name, e.g. Win32_BIOS</param>
+        /// <param name="wmiProperty">wmi property name in wmi class, i.e. SMBIOSBIOSVersion</param>
+        /// <returns>result as string if successfull, exception if failed for some reason</returns>
+        public string GetSingleDetail(string wmiClass, string wmiProperty)
+        {
+            GetHWinfo HW = new GetHWinfo();
+            return HW.identifier(wmiClass, wmiProperty);
+        }
+    }
+}
